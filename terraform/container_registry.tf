bd2cd444a2f2c0ae7f794fb00472bc225947147e
Gerald <geraldortiz1693@gmail.com>
2024-05-10 16:10:11 -0500

    fix terraform

--- terraform/container_registry.tf
+++ terraform/container_registry.tf
@@ -2,7 +2,7 @@ resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
   resource_group_name = azurerm_resource_group.rg_all.name
   location            = var.location
-  sku                 = "${acr.sk}"
+  sku                 = var.acr_sku
   admin_enabled       = false
 
 }
