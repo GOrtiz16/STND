3e9843c1897dd6d6d6c83a3aa2acfab8d584a87a
Gerald <geraldortiz1693@gmail.com>
2024-05-11 16:22:05 -0500

    acr

--- terraform/container_registry.tf
+++ terraform/container_registry.tf
@@ -15,33 +15,33 @@ resource "azurerm_role_assignment" "enablePulling" {
   skip_service_principal_aad_check = true
 }
 
-resource "azurerm_private_dns_zone" "acr" {
-  name                = "privatelink.azurecr.io"
-  resource_group_name = azurerm_resource_group.rg_all.name
-}
+# resource "azurerm_private_dns_zone" "acr" {
+#   name                = "privatelink.azurecr.io"
+#   resource_group_name = azurerm_resource_group.rg_all.name
+# }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "acr_vnet_link" {
-  name                  = "acr-link"
-  resource_group_name   = azurerm_resource_group.rg_all.name
-  private_dns_zone_name = azurerm_private_dns_zone.acr.name
-  virtual_network_id    = azurerm_virtual_network.vnet01.id
-}
+# resource "azurerm_private_dns_zone_virtual_network_link" "acr_vnet_link" {
+#   name                  = "acr-link"
+#   resource_group_name   = azurerm_resource_group.rg_all.name
+#   private_dns_zone_name = azurerm_private_dns_zone.acr.name
+#   virtual_network_id    = azurerm_virtual_network.vnet01.id
+# }
 
-resource "azurerm_private_endpoint" "az_acr_endpoint" {
-  name                = "private-endpoint-acr"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg_all.name
-  subnet_id           = azurerm_subnet.sbnt01.id
+# resource "azurerm_private_endpoint" "az_acr_endpoint" {
+#   name                = "private-endpoint-acr"
+#   location            = var.location
+#   resource_group_name = azurerm_resource_group.rg_all.name
+#   subnet_id           = azurerm_subnet.sbnt01.id
 
-  private_service_connection {
-    name                           = "private-serviceconnection"
-    private_connection_resource_id = azurerm_container_registry.acr.id
-    subresource_names              = ["registry"]
-    is_manual_connection           = false
-  }
+#   private_service_connection {
+#     name                           = "private-serviceconnection"
+#     private_connection_resource_id = azurerm_container_registry.acr.id
+#     subresource_names              = ["registry"]
+#     is_manual_connection           = false
+#   }
 
-  private_dns_zone_group {
-    name                 = "acr-dns-zone-group"
-    private_dns_zone_ids = [azurerm_private_dns_zone.acr.id]
-  }
-}
+#   private_dns_zone_group {
+#     name                 = "acr-dns-zone-group"
+#     private_dns_zone_ids = [azurerm_private_dns_zone.acr.id]
+#   }
+# }
